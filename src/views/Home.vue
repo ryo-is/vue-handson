3b309101d28bececd20e9547c56878422a6baafc
is_ryo <is_ryo@izuriyonoMacBook.local>
2020-02-14 23:20:15 +0900

    ちょっと追記

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <template lang="pug">
-  v-container.home Hello Vue!!!
+  v-container.home
+    v-row(justify="center", align-content="center", style="height: calc(100vh - 200px);")
+      v-col(cols=12)
+        .display-1 Hello Vue
+      v-col(cols=12)
+        v-icon add
+        v-icon mdi-mouse
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+
 export default Vue.extend({})
 </script>
 
